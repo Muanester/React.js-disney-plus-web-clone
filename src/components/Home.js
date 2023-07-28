ec73f92ab0fcc5f7ad65d6ae82541a8e9c4ab20b
Muanester <stevenmutisya999@gmail.com>
2023-07-28 15:12:57 +0300

    Add Image Slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { styled } from "styled-components";
+import ImgSlider from "./ImgSlider";
+import Viewers from "./Viewers";
 
 function Home() {
-  return <Container>Home</Container>;
+  return <Container>
+    <ImgSlider />
+  </Container>;
 }
 
 export default Home;
@@ -11,6 +15,7 @@ const Container = styled.main`
 min-height: calc(100vh - 70px);
 padding: 0 calc(3.5vw + 5px);
 position: relative;
+overflow-x: hidden;
 
 &:before {
 background: url('/images/home-background.png') center center / cover no-repeat fixed;
