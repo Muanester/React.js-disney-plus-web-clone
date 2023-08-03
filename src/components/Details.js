6ea91f1a012658eaf1d7da6cac3fd260614612cc
Muanester <stevenmutisya999@gmail.com>
2023-08-03 16:43:01 +0300

    Add Details

--- src/components/Details.js
+++ src/components/Details.js
@@ -10,6 +10,29 @@ function Details() {
       <ImageTitle>
         <img src="/images/logo.svg" />
       </ImageTitle>
+      <Controls>
+        <PlayButton>
+          <img src="/images/play-icon-black.png" />
+          <span>PLAY</span>
+        </PlayButton>
+        <TrailerButton>
+          <img src="/images/play-icon-white.png" />
+          <span>TRAILER</span>
+        </TrailerButton>
+        <AddButton>
+          <span>+</span>
+        </AddButton>
+        <GroupMatchButton>
+          <img src="/images/group-icon.png" />
+        </GroupMatchButton>
+      </Controls>
+      <SubTitle>Lorem ipsum dolor sit amet. Lorem, ipsum dolor.</SubTitle>
+      <Description>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.Lorem ipsum
+        dolor sit amet consectetur, adipisicing elit. Tempora, iure? A aut,
+        assumenda voluptatibus maiores repellendus ab velit magni maxime enim
+        veritatis!
+      </Description>
     </Container>
   );
 }
@@ -49,4 +72,64 @@ const ImageTitle = styled.div`
     height: 100%;
     object-fit: contain;
   }
+`;
+
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const PlayButton = styled.button`
+  border-radius: 4px;
+  font-size: 15px;
+  padding: 0 24px;
+  margin-right: 22px;
+  display: flex;
+  align-items: center;
+  height: 56px;
+  background: rgb(249, 249, 249);
+  border: none;
+  letter-spacing: 1.8px;
+  cursor: pointer;
+
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+`;
+const TrailerButton = styled(PlayButton)`
+  background: rgba(0, 0, 0, 0.5);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+`;
+const AddButton = styled.button`
+  margin-right: 16px;
+  width: 44px;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  border: 2px solid white;
+  background-color: rgba(0, 0, 0, 0.5);
+  cursor: pointer;
+
+  span {
+    font-size: 30px;
+    color: white;
+  }
+`;
+const GroupMatchButton = styled(AddButton)`
+  background: rgb(0, 0, 0);
+`;
+
+const SubTitle = styled.div`
+  color: rgb(249, 249, 249);
+  font-size: 15px;
+  min-height: 20px;
+  margin-top: 26px;
+`;
+const Description = styled.div`
+  line-height: 1.4;
+  font-size: 20px;
+  margin-top: 16px;
+  color: rgb(249, 249, 249);
 `;
